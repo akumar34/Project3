05c68a632c75da5f27c9891b29aff1d2ec2cf6f5
Nizobe <nbessi2@uic.edu>
2014-11-14 20:13:14 -0600

    Data as Markers!
    I have moved my work work for custom markers here to master now.
    Currently the CTA data has a placeholder marker and no popups are bound
    to CTA data. This will all be changed later when I have the chance. At
    least for now, it all works and it's just a series of cosmetic tweaks
    that needs to be made.

--- js/DataCircles.js
+++ js/DataCircles.js
@@ -8,6 +8,68 @@ function DataCircles() {
     
     var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S").parse;
 
+    //custom markers
+    //pothole
+    var potholeIcon = L.icon({
+        iconUrl: 'icons/svg/exclamation3.svg',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    });//end pothole
+
+    //abandoned vehicles
+    var abandonedVehicleIcon = L.icon({
+        iconUrl: 'icons/svg/car168.svg',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76] 
+    });//end abandoned vehicles
+
+    //divvy stations
+    var divvyStationIcon = L.icon({
+        iconUrl: 'icons/svg/pins6.svg',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    });//end divvy station
+
+    //street lights
+    var streetLightIcon = L.icon({
+        iconUrl: 'icons/svg/street9.svg',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76] 
+    });//end street lights
+
+    //crimes
+    var crimeIcon = L.icon({
+        iconUrl: 'icons/svg/handcuffs.svg',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76] 
+    });//end crimes
+
+    //CTA
+    var ctaIcon = L.icon({
+    iconUrl: 'icons/svg/pins17.svg',
+    iconSize:     [38, 95],
+    shadowSize:   [50, 64],
+    iconAnchor:   [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor:  [-3, -76]
+    });//end CTA
+    //end custom markers
+
   // add layers of data
     function addLayers(layersInfo, layers){
         for (var i = 0; i < layersInfo.length; i++) {
@@ -95,6 +157,20 @@ function DataCircles() {
                 if (data[i].status.indexOf("completed") > -1)
                     outLine = "white";
 
+                //check what type of data we're using so we know what icon to use.
+                switch(layerInfo.type) {
+                    case "Potholes":
+                        addPothole(index, i, data, layers, 0);
+                        break;
+                    case "Lights":
+                        addStreetLight(index, i, data, layers, 0);
+                        break;
+                    case "Abandoned Vehicles":
+                        addAbandonedVehicle(index, i, data, layers, 0);
+                        break;
+                }//end switch/case on what type of data we're importing
+
+                /*
                 layersContainer[index].circles.push(
                     L.circleMarker([data[i]["latitude"], data[i]["longitude"]], 
                         {
@@ -111,11 +187,12 @@ function DataCircles() {
                         "<br><strong>Street Address:</strong> " + data[i]["street_address"] + "<br><strong>Status:</strong> " +
                         data[i]["status"] + "<br><strong>Creation Date:</strong> " + data[i]["creation_date"].substring(0,10))
                     /*binds popup to the pothole markers pulling each bit of relevant data. on the last part, the substring just cuts off
-                    the extraneous 0'd out time that's appended after the date in each entry*/
+                    the extraneous 0'd out time that's appended after the date in each entry
                 );
+                */
             };
 
-            L.layerGroup(layersContainer[index].circles).addTo(layers);
+            //L.layerGroup(layersContainer[index].circles).addTo(layers);
 			// layersContainer[index].refresh = parseDate(data[refreshIndex].creation_date);
         });
     };
@@ -138,6 +215,10 @@ function DataCircles() {
 
                 // add the circles
                 outLine = layerInfo.color[statusValue];
+
+                addDivvyStation(index, i, data, layers, 0);
+
+                /*
                 layersContainer[index].circles.push(
                     L.circleMarker([data[i]["latitude"], data[i]["longitude"]], 
                         {
@@ -156,8 +237,9 @@ function DataCircles() {
                         data[i]["statusValue"] +"<br><strong>Occupied Docks / Total Docks: </strong>" + data[i]["availableBikes"] + 
                         "/" + data[i]["totalDocks"])
                 );
-            };
-		L.layerGroup(layersContainer[index].circles).addTo(layers);
+                */
+            };//end for loop
+		//L.layerGroup(layersContainer[index].circles).addTo(layers);
 		//layersContainer[index].refresh = parseDate(data[refreshIndex].executionTime);
 		}
     };
@@ -189,7 +271,10 @@ function DataCircles() {
                 if (daysAgo >= 14)
                     outLine = layerInfo.color;
 
-				layersContainer[index].circles.push(
+                addCrime(index, i, data, layers, 0);
+				
+                /*
+                layersContainer[index].circles.push(
 					L.circleMarker([data[i]["latitude"], data[i]["longitude"]], 
 						{
 							zindex: 10,
@@ -203,9 +288,10 @@ function DataCircles() {
 						}
 					)
                 );
-			};
+                */
+			};//end for loop
 
-            L.layerGroup(layersContainer[index].circles).addTo(layers);
+            //L.layerGroup(layersContainer[index].circles).addTo(layers);
 			// layersContainer[index].refresh = parseDate(data[refreshIndex].creation_date);
         });
     };
@@ -235,7 +321,10 @@ function DataCircles() {
 
 							if ( data[i]["lat"] == undefined || data[i]["lon"] == undefined) continue;
 
-							layersContainer[index].circles.push(
+							addCTA(index, i, data, layers, 0);
+
+                            /*
+                            layersContainer[index].circles.push(
 								L.circleMarker([data[i]["lat"], data[i]["lon"]], 
 									{
 										zindex: 10,
@@ -249,11 +338,12 @@ function DataCircles() {
 										lon : data[i].lon
 									}
 								)
-							);
+							);//end .push
+                            */
 						};
 					};
 
-					L.layerGroup(layersContainer[index].circles).addTo(layers);
+					//L.layerGroup(layersContainer[index].circles).addTo(layers);
 					// layersContainer[index].refresh = parseDate(data[refreshIndex].creation_date);
 				});
 			}
@@ -297,6 +387,20 @@ function DataCircles() {
                 if (data[i].status.indexOf("completed") > -1)
                     outLine = "white";
 				
+                //check what type of data we're using so we know what icon to use.
+                switch(layerInfo.type) {
+                    case "Potholes":
+                        addPothole(index, i, data, layers, 1);
+                        break;
+                    case "Lights":
+                        addStreetLight(index, i, data, layers, 1);
+                        break;
+                    case "Abandoned Vehicles":
+                        addAbandonedVehicle(index, i, data, layers, 1);
+                        break;
+                }//end switch/case on what type of data we're importing
+
+                /*
                 layersContainer[index].circles.push(
                     L.circleMarker([data[i]["latitude"], data[i]["longitude"]], 
                     {
@@ -311,11 +415,12 @@ function DataCircles() {
                         service_request_number : data[i].service_request_number
                     }
                 ));
-            };
+                */
+            };//end for loop
 
             //if( layersContainer[index].refresh === null ) )
-            layers.clearLayers();
-			L.layerGroup(layersContainer[index].circles).addTo(layers);
+            //layers.clearLayers();
+			//L.layerGroup(layersContainer[index].circles).addTo(layers);
 			// layersContainer[index].refresh = parseDate(data[refreshIndex].creation_date);
         });
     };
@@ -342,6 +447,10 @@ function DataCircles() {
 
                 // add the circles
                 outLine = layerInfo.color[statusValue];
+
+                addDivvyStation(index, i, data, layers, 1);
+
+                /*
                 layersContainer[index].circles.push(
                     L.circleMarker([data[i]["latitude"], data[i]["longitude"]], 
                     {
@@ -357,10 +466,11 @@ function DataCircles() {
 						statusValue : data[i].statusValue
                     }
                 ));
+                */
             };
             //if( layersContainer[index].refresh === null ) )
-            layers.clearLayers();
-			L.layerGroup(layersContainer[index].circles).addTo(layers);
+            //layers.clearLayers();
+			//L.layerGroup(layersContainer[index].circles).addTo(layers);
 			// layersContainer[index].refresh = parseDate(data[refreshIndex].creation_date);
 		}
     };
@@ -383,7 +493,9 @@ function DataCircles() {
                 if ( data[i]["latitude"] == undefined || data[i]["longitude"] == undefined) continue;
 
                 // add the circles
+                addCrime(index, i, data, layers, 1);
 
+                /*
                 layersContainer[index].circles.push(
                     L.circleMarker([data[i]["latitude"], data[i]["longitude"]], 
                     {
@@ -397,11 +509,12 @@ function DataCircles() {
 						id : data[i].id
                     }
                 ));
-            };
+                */
+            };//end for loop
 
             //if( layersContainer[index].refresh === null ) )
-            layers.clearLayers();
-			L.layerGroup(layersContainer[index].circles).addTo(layers);
+            //layers.clearLayers();
+			//L.layerGroup(layersContainer[index].circles).addTo(layers);
 			// layersContainer[index].refresh = parseDate(data[refreshIndex].creation_date);
         });
     };
@@ -434,7 +547,10 @@ function DataCircles() {
 							 
 							if ( data[i]["lat"] == undefined || data[i]["lon"] == undefined) continue;
 
-							layersContainer[index].circles.push(
+							addCTA(index, i, data, layers, 1);
+
+                            /*
+                            layersContainer[index].circles.push(
 								L.circleMarker([data[i]["lat"], data[i]["lon"]], 
 									{
 										zindex: 10,
@@ -448,11 +564,12 @@ function DataCircles() {
 										lon : data[i].lon
 									}
 								)
-							);
+							);// end .push
+                            */
 						};
 					};
 
-					L.layerGroup(layersContainer[index].circles).addTo(layers);
+					//L.layerGroup(layersContainer[index].circles).addTo(layers);
 					// layersContainer[index].refresh = parseDate(data[refreshIndex].creation_date);
 				});
 			}
@@ -484,7 +601,8 @@ function DataCircles() {
 
                 if (!gju.pointInPolygon(point, poly)){
                     // this needs to be changed later!
-                    layersContainer[q].circles[i].setStyle({opacity: 0, fillOpacity:0});
+                    layersContainer[q].circles[i].setOpacity(0);
+                    //layersContainer[q].circles[i].setStyle({opacity: 0, fillOpacity:0});
                 }
                 else{
                     // create subset of the total circles to be later shown on map and sent to graphs
@@ -578,6 +696,126 @@ function DataCircles() {
         };
         return null;
     };
+
+    //helper function for adding potholes to map
+    function addPothole(layerIndex, dataIndex, data, layers, refresh) {
+        layersContainer[layerIndex].circles.push(
+            L.marker([data[dataIndex]["latitude"], data[dataIndex]["longitude"]], 
+            {
+                icon: potholeIcon
+            }
+        ).bindPopup("<strong>Community Area:</strong> " + data[dataIndex]["community_area"] +
+            "<br><strong>Street Address:</strong> " + data[dataIndex]["street_address"] + "<br><strong>Status:</strong> " +
+            data[dataIndex]["status"] + "<br><strong>Creation Date:</strong> " + data[dataIndex]["creation_date"].substring(0,10))//because time at end is always uselessly zeroed
+        );//end .push
+        //TODO change icon based on value of "status"
+        if(refresh == 1) {
+            layers.clearLayers();
+        }
+        L.layerGroup(layersContainer[layerIndex].circles).addTo(layers);
+        return null;
+    };//end addPothole()
+
+    //helper function for adding street light data to map
+    function addStreetLight(layerIndex, dataIndex, data, layers, refresh) {
+        layersContainer[layerIndex].circles.push(
+            L.marker([data[dataIndex]["latitude"], data[dataIndex]["longitude"]], 
+            {
+                icon: streetLightIcon
+            }
+        ).bindPopup("<strong>Community Area:</strong> " + data[dataIndex]["community_area"] +
+            "<br><strong>Street Address:</strong> " + data[dataIndex]["street_address"] + "<br><strong>Status:</strong> " +
+            data[dataIndex]["status"] + "<br><strong>Creation Date:</strong> " + data[dataIndex]["creation_date"].substring(0,10))
+        );//end .push
+        //TODO change icon based on value of "status"
+        if(refresh == 1) {
+            layers.clearLayers();
+        }
+        L.layerGroup(layersContainer[layerIndex].circles).addTo(layers);
+        return null;
+    };//end addStreetLight()
+
+    //helper function for adding abandoned vehicle data to map
+    function addAbandonedVehicle(layerIndex, dataIndex, data, layers, refresh) {
+        layersContainer[layerIndex].circles.push(
+            L.marker([data[dataIndex]["latitude"], data[dataIndex]["longitude"]], 
+            {
+                icon: abandonedVehicleIcon
+            }
+        ).bindPopup("<strong>Community Area:</strong> " + data[dataIndex]["community_area"] +
+            "<br><strong>Street Address:</strong> " + data[dataIndex]["street_address"] + "<br><strong>Status:</strong> " +
+            data[dataIndex]["status"] + "<br><strong>Creation Date:</strong> " + data[dataIndex]["creation_date"].substring(0,10) + 
+            "<br><strong>Days Reported Parked:</strong> " + data[dataIndex]["how_many_days_has_the_vehicle_been_reported_as_parked_"])
+        );//end .push
+        //TODO change icon based on value of "status"
+        //TODO do we want to put extra data like color, licenese plate, make/model? maybe under a "show more" tab/button, etc?
+        if(refresh == 1) {
+            layers.clearLayers();
+        }
+        L.layerGroup(layersContainer[layerIndex].circles).addTo(layers);
+        return null;
+    };//end addAbandonedVehicle()
+
+    //helper function for adding divvy stations to map
+    function addDivvyStation(layerIndex, dataIndex, data, layers, refresh) {
+        layersContainer[layerIndex].circles.push(
+            L.marker([data[dataIndex]["latitude"], data[dataIndex]["longitude"]], 
+            {
+                icon: divvyStationIcon
+            }
+        ).bindPopup("<strong>Station Name:</strong> " + data[dataIndex]["stationName"] + "<br><strong>Status:</strong> " +
+                data[dataIndex]["statusValue"] +"<br><strong>Occupied Docks / Total Docks: </strong>" + data[dataIndex]["availableBikes"] + 
+                "/" + data[dataIndex]["totalDocks"])
+        );//end .push
+        //TODO change icon based on value of "statusValue"
+        if(refresh == 1) {
+            layers.clearLayers();
+        }
+        L.layerGroup(layersContainer[layerIndex].circles).addTo(layers);
+        return null;
+    };//end addDivvyStation()
+
+    //helper function for adding crime data to map
+    function addCrime(layerIndex, dataIndex, data, layers, refresh) {
+        layersContainer[layerIndex].circles.push(
+            L.marker([data[dataIndex]["latitude"], data[dataIndex]["longitude"]], 
+            {
+                icon: crimeIcon
+            }
+        ).bindPopup("<strong>Type:</strong> " + data[dataIndex]["primary_type"] + "<br><strong>Arrest:</strong> " +
+                data[dataIndex]["arrest"] +"<br><strong>Location Description:</strong> " + data[dataIndex]["location_description"] + 
+                "<br><strong>Date:</strong> " + data[dataIndex]["date"].substring(0,10) + "<br><strong>Updated On:</strong> " +
+                data[dataIndex]["updated_on"])
+        );//end .push
+        //TODO maybe add details like the description and if it was domestic or not, case number, iucr, etc? either
+        //in the popup or some sort of "show more"? or maybe we show some basic info on hover, and that+more on click/popup?
+        if(refresh == 1) {
+            layers.clearLayers();
+        }
+        L.layerGroup(layersContainer[layerIndex].circles).addTo(layers);
+        return null;
+    };//end addCrime()
+
+    //helper function for adding CTA data to map
+    function addCTA(layerIndex, dataIndex, data, layers, refresh) {
+    layersContainer[layerIndex].circles.push(
+        L.marker([data[dataIndex]["lat"], data[dataIndex]["lon"]], 
+        {
+            icon: ctaIcon
+        }
+    )/*.bindPopup("<strong>Community Area:</strong> " + data[dataIndex]["community_area"] +
+        "<br><strong>Street Address:</strong> " + data[dataIndex]["street_address"] + "<br><strong>Status:</strong> " +
+        data[dataIndex]["status"] + "<br><strong>Creation Date:</strong> " + data[dataIndex]["creation_date"].substring(0,10) + 
+        "<br><strong>Days Reported Parked:</strong> " + data[dataIndex]["how_many_days_has_the_vehicle_been_reported_as_parked_"])*/
+    );//end .push
+    //TODO change icon based on value of "status"
+    //TODO do we want to put extra data like color, licenese plate, make/model? maybe under a "show more" tab/button, etc?
+    if(refresh == 1) {
+        layers.clearLayers();
+    }
+    L.layerGroup(layersContainer[layerIndex].circles).addTo(layers);
+    return null;
+};//end addCTA()
 	
     DataCirclesObj.filterByShape = filterByShape;
     DataCirclesObj.addLayers = addLayers;
